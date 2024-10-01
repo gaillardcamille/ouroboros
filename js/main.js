d51a9f966307b3889da8c0cb8c07f896bcaf6138
gaillardcamille <gaillard.camille48@gmail.com>
2024-10-01 11:46:52 -0400

    début téléphone

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 $(document).ready(function () {
 
 
+	var nombreMessage = "3";
+	$("#nombreMessage").text(nombreMessage);
+
 	$("#debutJeu").on("click", function () {
 		$("#debutJeu").css("display", "none")
 	});
 
+	$("#ouvrirTelephone").on("click", function () {
+		$("#telephone").css("bottom", "50px")
+		$("#backgroundTelephone").css("display", "block")
+	});
+
+	$("#backgroundTelephone").on("click", function () {
+		$("#telephone").css("bottom", "-99%")
+		$("#backgroundTelephone").css("display", "none")
+	});
+
+	$("#verouillageTelephone").on("click", function () {
+		$("#verouillageTelephone").css("top", "-100%")
+	});
+	
 });
